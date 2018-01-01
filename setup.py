fec0beb55b65d78281f4cf92fb051e0639e97c9c
Naoaki Obiki <obiki.wk@gmail.com>
2018-01-01 23:21:56 +0900

    modified:   setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
+# coding: utf-8
 
 from distutils.core import setup
 
-setup(name="Distutils",
+setup(name="Gaccho",
       version="0.0.3",
       description="timeline aggregation tool for Rss, Twitter, and more",
       author="Naoaki Obiki",
       author_email="obiki.wk@gmail.com",
       url="https://github.com/nobiki/gaccho",
-      packages=["distutils", "distutils.command"],
+      packages=["gaccho"],
      )
